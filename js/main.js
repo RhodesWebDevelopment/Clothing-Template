7d043a6d2a39742819f29632d5118f4be16f57e2
Kenneth Rhodes <KennethRhodes8213@gmail.com>
2018-04-25 14:40:34 -0400

    Fixed a few things and closed #5

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function showButton() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+		document.getElementById("topBtn").style.display = "flex";
+	}else{
+		document.getElementById("topBtn").style.display = "none";
+	}
+}
+
+window.onscroll = function() {showButton()};
+
+function returnToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
