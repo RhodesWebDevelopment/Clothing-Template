6503a5a6a106e0e7ad7eceb6345dc0b80e77c783
Kenneth Rhodes <KennethRhodes8213@gmail.com>
2018-04-26 12:04:32 -0400

    Added loading screen.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-function showButton() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-		document.getElementById("topBtn").style.display = "flex";
-	}else{
-		document.getElementById("topBtn").style.display = "none";
+
+	function showButton() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+			document.getElementById("topBtn").style.display = "flex";
+		}else{
+			document.getElementById("topBtn").style.display = "none";
+		}
 	}
-}
 
-window.onscroll = function() {showButton()};
+	window.onscroll = function() {showButton()};
 
-function returnToTop() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-}
+	function returnToTop() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+	
+	function removeLoadingSplash() {
+		$("#loadingSplash").hide();
+	}
